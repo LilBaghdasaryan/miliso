187d091d9b18f64f886c9c403e1251b827425c54
Lilit <lilit.harutyunyann@gmail.com>
2025-03-20 21:39:13 +0400

    Created product page.

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,38 +1,38 @@
-import { createClient } from 'next-sanity'
+import { createClient } from "next-sanity";
 
-import { apiVersion, dataset, projectId } from '../env'
-import { sanityFetch } from '@/sanity/lib/live'
-import { Product, ProductCategory } from '@/sanity.types'
+import { apiVersion, dataset, projectId } from "../env";
+import { sanityFetch } from "@/sanity/lib/live";
+import { Product, ProductCategory } from "@/sanity.types";
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
+});
 
 export const getAllProducts = async () => {
-  const query = `*[_type == "product"]`
-  const products = await sanityFetch({ query: query })
-  return products.data as Product[] ;
-}
+  const query = `*[_type == "product"]`;
+  const products = await sanityFetch({ query: query });
+  return products.data as Product[];
+};
 
 export const getAllCategories = async () => {
-  const query = `*[_type == "productCategory"]`
-  const categories = await sanityFetch({ query: query })
-  return categories.data as ProductCategory[] ;
-}
+  const query = `*[_type == "productCategory"]`;
+  const categories = await sanityFetch({ query: query });
+  return categories.data as ProductCategory[];
+};
 
 export const getCategoryBySlug = async (slug: string) => {
-  const  query = `*[_type == "productCategory" && slug.current == $slug][0]`
-  const category = await sanityFetch({ query: query, params: { slug } })
-  return category.data as ProductCategory ;
-}
+  const query = `*[_type == "productCategory" && slug.current == $slug][0]`;
+  const category = await sanityFetch({ query: query, params: { slug } });
+  return category.data as ProductCategory;
+};
 export const getProductsByCategorySlug = async (slug: string) => {
-  const query = `*[_type == "product" && references(*[_type == "productCategory" && slug.current == $slug][0]._id)]`
-  const products = await sanityFetch({ query: query, params: { slug } })
-  return products.data as ProductCategory ;
-}
+  const query = `*[_type == "product" && references(*[_type == "productCategory" && slug.current == $slug][0]._id)]`;
+  const products = await sanityFetch({ query: query, params: { slug } });
+  return products.data as ProductCategory;
+};
 
 export const searchProducts = async (searchQuery: string) => {
   const query = `*[_type == "product" && (
@@ -41,6 +41,6 @@ export const searchProducts = async (searchQuery: string) => {
     category->title match "*" + $searchQuery + "*" ||
     category->slug.current match "*" + $searchQuery + "*")]`;
 
-  const products = await sanityFetch({ query: query, params: { searchQuery } })
-  return products.data as Product[] ;
-}
+  const products = await sanityFetch({ query: query, params: { searchQuery } });
+  return products.data as Product[];
+};
